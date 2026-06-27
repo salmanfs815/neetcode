51dec99d54500e08d90cc021a38c1a2e563acbd7
Salman Siddiqui <salmanf.siddiqui@gmail.com>
2026-06-27 14:39:58 -0700

    Enhance trie.py with explanatory comments
    Added comments to explain the Trie data structure and its operations.

--- trie.py
+++ trie.py
@@ -1,5 +1,9 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/description/
 
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+# Trie can perform the following operations in O(1) time: insert word, search word, search for a prefix.
+# Each TrieNode is a character with 0-26 children and a boolean to mark the end of word.
+
 class TrieNode:
     def __init__(self):
         self.children = {}

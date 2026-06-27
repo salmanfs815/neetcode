9bc4b22a8476d30e0bc351f7d632bee5e2047294
Salman Siddiqui <salmanf.siddiqui@gmail.com>
2026-06-27 14:49:40 -0700

    Document Union-Find class with detailed comments
    Added comments explaining the Union-Find data structure and its operations.

--- union_find.py
+++ union_find.py
@@ -1,5 +1,16 @@
 # https://neetcode.io/problems/unionFind/question
 
+# Union-Find (aka Disjoint Set) is a tool for finding disjoint sets of vertices in a graph or for cycle detection.
+# Idea: iterate over graph's edges and union root parents of both vertices to make them part of the same connected component.
+# Runtime is improved from O(n) for basic solution to ~ O(1) with union by rank (join smaller component into larger one) and path compression (make nodes in component direct children of root) optimizations.
+
+# UnionFind class operations:
+#   * UnionFind(int n) will initialize a disjoint set of size n.
+#   * int find(int x) will return the root of the component that x belongs to.
+#   * bool isSameComponent(int x, int y) will return whether x and y belong to the same component.
+#   * bool union(int x, int y) will union the components that x and y belong to. If they are already in the same component, return false, otherwise return true.
+#   * int getNumComponents() will return the number of components in the disjoint set.
+
 class UnionFind:
     
     def __init__(self, n: int):
